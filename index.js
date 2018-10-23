177daff3946cec16a09caa57473f48b559bd21df
Cody S <sheridan.codyj@gmail.com>
2018-10-23 06:14:42 -0400

    Adding sound

--- index.js
+++
@@ -1,6 +0,0 @@
-function onKeyDown(event) {
-  var maxPoint = new Point(view.size.width, view.size.height);
-  var randomPoint = point.random();
-  var point = maxPoint * randomPoint;
-  new Path.Circle(maxPoint, 10).fillColor = 'cyan';
-}
